0b181dab30f1d827753ccdaeb6b156e4e0ab4edb
burckhju <juergen.burckhardt@gmx.de>
2026-08-28 22:01:40 +0200

    FT-013: register proposal approval readback API

--- backend/app/features/model/api/__init__.py
+++ backend/app/features/model/api/__init__.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter
 
 from app.features.model.api.hypothesis_proposal_router import router as hypothesis_proposal_router
 from app.features.model.api.lesson_hypothesis_router import router as lesson_hypothesis_router
+from app.features.model.api.proposal_approval_router import router as proposal_approval_router
 from app.features.model.api.proposal_validation_router import router as proposal_validation_router
 from app.features.model.api.router import router as governance_router
 
@@ -12,5 +13,6 @@ router.include_router(governance_router)
 router.include_router(lesson_hypothesis_router)
 router.include_router(hypothesis_proposal_router)
 router.include_router(proposal_validation_router)
+router.include_router(proposal_approval_router)
 
 __all__ = ["router"]
