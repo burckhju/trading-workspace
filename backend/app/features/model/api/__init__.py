a0133ec671666e1fa1fcf863960a1ec3b5e8fbdc
burckhju <juergen.burckhardt@gmx.de>
2026-08-28 22:30:21 +0200

    FT-013: register runtime activation API

--- backend/app/features/model/api/__init__.py
+++ backend/app/features/model/api/__init__.py
@@ -7,6 +7,7 @@ from app.features.model.api.lesson_hypothesis_router import router as lesson_hyp
 from app.features.model.api.proposal_approval_router import router as proposal_approval_router
 from app.features.model.api.proposal_validation_router import router as proposal_validation_router
 from app.features.model.api.router import router as governance_router
+from app.features.model.api.runtime_activation_router import router as runtime_activation_router
 
 router = APIRouter()
 router.include_router(governance_router)
@@ -14,5 +15,6 @@ router.include_router(lesson_hypothesis_router)
 router.include_router(hypothesis_proposal_router)
 router.include_router(proposal_validation_router)
 router.include_router(proposal_approval_router)
+router.include_router(runtime_activation_router)
 
 __all__ = ["router"]
